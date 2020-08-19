7a15f29ec01a778683a1d8155d59f6c7e44d9c2e
Andres Pineda <pineda.andres@gmail.com>
2020-08-19 02:25:41 -0400

    Initial Commit

---
+++ ButtonVisibility.Shared/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ButtonVisibility.Shared
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool isModifiable = true;
+        public bool IsModifiable
+        {
+            get => isModifiable;
+            set
+            {
+                isModifiable = value;
+                RaisePropertyChanged(nameof(IsModifiable));
+            }
+        }
+
+        public MainViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
